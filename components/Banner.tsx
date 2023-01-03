f1bf546d57332886ad4647a6399a1053dfb95934
Daniel Brož <danobroz66@gmail.com>
2023-01-03 17:13:19 +0100

    add handle click function from parent

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -7,11 +7,6 @@ interface ButtonProps {
 }
 
 export default function Banner({ buttonText, buttonFunction }: ButtonProps) {
-  const handleClick = (e: MouseEvent) => {
-    e.preventDefault();
-    console.log("You clicked the button!");
-  };
-
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
@@ -19,7 +14,7 @@ export default function Banner({ buttonText, buttonFunction }: ButtonProps) {
         <span className={styles.title2}>Connoisseur</span>
       </h1>
       <p className={styles.subtitle}>Discover your local coffee shops!</p>
-      <button className={styles.button} onClick={handleClick}>
+      <button className={styles.button} onClick={buttonFunction}>
         {buttonText}
       </button>
     </div>
