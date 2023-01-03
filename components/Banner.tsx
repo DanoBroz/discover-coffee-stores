9642e3446f2a2fae6562f5693e83b319f60c5d97
Daniel Brož <danobroz66@gmail.com>
2023-01-03 19:17:02 +0100

    style banner and add root styling

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,21 +2,23 @@ import { MouseEvent } from "react";
 import styles from "./Banner.module.css";
 
 interface ButtonProps {
-  buttonText: string;
-  buttonFunction: (e: MouseEvent) => void;
+    buttonText: string;
+    buttonFunction: (e: MouseEvent) => void;
 }
 
 export default function Banner({ buttonText, buttonFunction }: ButtonProps) {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>
-        <span className={styles.title1}>Coffee</span>
-        <span className={styles.title2}>Connoisseur</span>
-      </h1>
-      <p className={styles.subtitle}>Discover your local coffee shops!</p>
-      <button className={styles.button} onClick={buttonFunction}>
-        {buttonText}
-      </button>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                <span className={styles.title1}>Coffee</span>
+                <span className={styles.title2}>Connoisseur</span>
+            </h1>
+            <p className={styles.subtitle}>Discover your local coffee shops!</p>
+            <div className={styles.buttonWrapper}>
+                <button className={styles.button} onClick={buttonFunction}>
+                    {buttonText}
+                </button>
+            </div>
+        </div>
+    );
 }
