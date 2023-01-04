380bbc6555b3be32656938fcdb312c819993afb0
Daniel Brož <danobroz66@gmail.com>
2023-01-04 17:35:40 +0100

    add google fonts with next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { IBM_Plex_Sans } from "@next/font/google";
+import React from "react";
+
+const ibmPlexSans = IBM_Plex_Sans({
+    weight: ["400", "600", "700"],
+    subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <footer>Hello there</footer>
-    </>
-  );
+    return (
+        <div className={ibmPlexSans.className}>
+            <Component {...pageProps} />
+            <footer>Hello there</footer>
+        </div>
+    );
 }
