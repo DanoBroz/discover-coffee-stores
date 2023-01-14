d101a82840065c37a4e7dee01a3dd19c5d393837
Daniel Brož <danobroz66@gmail.com>
2023-01-14 07:47:54 +0100

    return dummy data from api

--- pages/api/createCoffeeStore.ts
+++ pages/api/createCoffeeStore.ts
@@ -1,6 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
 const Airtable = require("airtable");
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
     process.env.AIRTABLE_BASE_KEY
 );
 
 const table = base("coffee-stores");
+
+console.log({ table });
+
+const createCoffeeStore = async (req: NextApiRequest, res: NextApiResponse) => {
+    res.status(200).json({ msg: "hello" });
+};
+
+export default createCoffeeStore;
