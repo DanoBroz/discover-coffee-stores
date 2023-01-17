18184f1c1a1b9f3c8c276073ddc879adce677577
Daniel Brož <danobroz66@gmail.com>
2023-01-17 22:02:49 +0100

    find coffee store record

--- pages/api/createCoffeeStore.ts
+++ pages/api/createCoffeeStore.ts
@@ -4,8 +4,28 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
     process.env.AIRTABLE_BASE_KEY
 );
 
+const table = base("coffee-stores");
+
 const createCoffeeStore = async (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json({ msg: "hello" });
+    const findCoffeeStoreRecords = await table
+        .select({
+            filterByFormula: `id="0"`,
+        })
+        .firstPage();
+
+    console.log({ findCoffeeStoreRecords });
+
+    if (req.method === "POST") {
+        if (findCoffeeStoreRecords.length > 0) {
+            // const re
+            // findCoffeeStoreRecords.map(record => )
+            res.json(findCoffeeStoreRecords);
+        } else {
+            res.json({ msg: "no records found" });
+        }
+    }
+
+    // res.status(200).json({ msg: "hello" });
 };
 
 export default createCoffeeStore;
